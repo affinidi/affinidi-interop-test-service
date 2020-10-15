b8a76811939d9587ba69ee48ed14c42ff19a5f7c
Afshan Aman <afshan@bloom.co>
2020-10-15 08:06:37 -0600

    printing the INTEROP_SECRETS

--- apps/server/src/interop/interop.test.ts
+++ apps/server/src/interop/interop.test.ts
@@ -5,8 +5,10 @@ import { logger } from '../shared/logger'
 const request = supertest(app)
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 50000
 
-logger.info(process.env)
-logger.info(process.env.INTEROP_SECRETS)
+logger.info(JSON.parse(process.env.INTEROP_SECRETS))
+
+const SECRETS = JSON.parse(process.env.INTEROP_SECRETS)
+logger.info(SECRETS.DID)
 
 describe('Dummy Test ', () => {
   test('should be true', () => {
