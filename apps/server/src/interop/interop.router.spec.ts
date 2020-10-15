8f3175026ff6ac0dcb92a2d9f56931b2c4663088
Afshan Aman <afshan@bloom.co>
2020-10-15 14:19:40 -0600

    run only the failing test

--- apps/server/src/interop/interop.router.spec.ts
+++ apps/server/src/interop/interop.router.spec.ts
@@ -327,100 +327,100 @@ describe('Integration Tests: Interop API Router', () => {
     })
   })
 
-  // describe('POST /api/v1/interop/verify-presentation', () => {
-  // describe('Succcess Case:', () => {
-  //   test('should respond with status true, when VP is verified', async () => {
-  //     let vp
-  //     // Pre-requisites
-  //     try {
-  //       // step 1: get payload URL for QR code (this is to be called from the Verifier app)
-  //       const response = await request
-  //         .post('/api/v1/interop/presentation-challenge')
-  //         .set('Accept', 'application/json')
-  //         .send(requestPresentationChallenge)
-  //         .expect(200)
+  describe('POST /api/v1/interop/verify-presentation', () => {
+    describe('Succcess Case:', () => {
+      test('should respond with status true, when VP is verified', async () => {
+        let vp
+        // Pre-requisites
+        try {
+          // step 1: get payload URL for QR code (this is to be called from the Verifier app)
+          const response = await request
+            .post('/api/v1/interop/presentation-challenge')
+            .set('Accept', 'application/json')
+            .send(requestPresentationChallenge)
+            .expect(200)
 
-  //       const { tokenUrl } = response.body
-  //       logger.info('Step 1: tokenUrl: ', tokenUrl)
+          const { tokenUrl } = response.body
+          logger.info('Step 1: tokenUrl: ', tokenUrl)
 
-  //       if (tokenUrl) {
-  //         const uuid = tokenUrl.split('/').pop()
+          if (tokenUrl) {
+            const uuid = tokenUrl.split('/').pop()
 
-  //         // step 2: get presentation challenge (this is to be called from the Verifier app)
-  //         const response1 = await request
-  //           .get(`/api/v1/interop/presentation-challenge/${uuid}`)
-  //           .set('Accept', 'application/json')
-  //           .expect(200)
+            // step 2: get presentation challenge (this is to be called from the Verifier app)
+            const response1 = await request
+              .get(`/api/v1/interop/presentation-challenge/${uuid}`)
+              .set('Accept', 'application/json')
+              .expect(200)
 
-  //         const presentationChallenge = response1.body.token
-  //         logger.info('Step 2: presentationChallenge ')
-  //         logger.info(presentationChallenge)
+            const presentationChallenge = response1.body.token
+            logger.info('Step 2: presentationChallenge ')
+            logger.info(presentationChallenge)
 
-  //         // step 3: retrieve VC from vault (this part is to be implemented by the Wallet app)
+            // step 3: retrieve VC from vault (this part is to be implemented by the Wallet app)
 
-  //         // since this api doesnt have a VC stored in any vault, the workaround is to generate a VC on behalf of the Issuer, on the fly
-  //         const { password, encryptedSeedElem, encryptedSeedJolo } = getOptionsForEnvironment(ENVIRONMENT)
-  //         const affinity = new Affinity()
-  //         const vc = await affinity.signCredential(
-  //           buildVCV1Unsigned({
-  //             skeleton: buildVCV1Skeleton<VCSPhonePersonV1>({
-  //               id:                '123',
-  //               credentialSubject: {
-  //                 data: {
-  //                   '@type':   ['Person', 'PersonE', 'PhonePerson'],
-  //                   telephone: '555 555 5555'
-  //                 }
-  //               },
-  //               holder:  { id: didElem },
-  //               type:    'PhoneCredentialPersonV1',
-  //               context: getVCPhonePersonV1Context()
-  //             }),
-  //             issuanceDate: new Date().toISOString()
-  //           }),
-  //           encryptedSeedJolo,
-  //           password
-  //         )
+            // since this api doesnt have a VC stored in any vault, the workaround is to generate a VC on behalf of the Issuer, on the fly
+            const { password, encryptedSeedElem, encryptedSeedJolo } = getOptionsForEnvironment(ENVIRONMENT)
+            const affinity = new Affinity()
+            const vc = await affinity.signCredential(
+              buildVCV1Unsigned({
+                skeleton: buildVCV1Skeleton<VCSPhonePersonV1>({
+                  id:                '123',
+                  credentialSubject: {
+                    data: {
+                      '@type':   ['Person', 'PersonE', 'PhonePerson'],
+                      telephone: '555 555 5555'
+                    }
+                  },
+                  holder:  { id: didElem },
+                  type:    'PhoneCredentialPersonV1',
+                  context: getVCPhonePersonV1Context()
+                }),
+                issuanceDate: new Date().toISOString()
+              }),
+              encryptedSeedJolo,
+              password
+            )
 
-  //         logger.info('Step 3: vc ')
-  //         logger.info(vc)
+            logger.info('Step 3: vc ')
+            logger.info(vc)
 
-  //         // step 4: generate VP (this part is to be implemented by the Wallet app)
-  //         const walletCommonNetworkMember = new CoreNetwork(password, encryptedSeedElem)
-  //         vp = await walletCommonNetworkMember.createPresentationFromChallenge(
-  //           presentationChallenge,
-  //           [vc],
-  //           'domain')
+            // step 4: generate VP (this part is to be implemented by the Wallet app)
+            const walletCommonNetworkMember = new CoreNetwork(password, encryptedSeedElem)
+            vp = await walletCommonNetworkMember.createPresentationFromChallenge(
+              presentationChallenge,
+              [vc],
+              'domain')
 
-  //         logger.info('Step 4: vp ')
-  //         logger.info(vp)
-  //       } else {
-  //         console.log('Payload URL was not found')
-  //       }
-  //     } catch (e) {
-  //       console.log(e.message)
-  //     }
+            logger.info('Step 4: vp ')
+            logger.info(vp)
+          } else {
+            console.log('Payload URL was not found')
+          }
+        } catch (e) {
+          console.log(e.message)
+        }
 
-  //     // Test the endpoint
-  //     const requestVerifyPresentation: InputVerifyPresentation = {
-  //       vp
-  //     }
-  //     const response = await request
-  //       .post('/api/v1/interop/verify-presentation')
-  //       .set('Accept', 'application/json')
-  //       .send(requestVerifyPresentation)
-  //       .expect(200)
+        // Test the endpoint
+        const requestVerifyPresentation: InputVerifyPresentation = {
+          vp
+        }
+        const response = await request
+          .post('/api/v1/interop/verify-presentation')
+          .set('Accept', 'application/json')
+          .send(requestVerifyPresentation)
+          .expect(200)
 
-  //     logger.info('Step test: response ')
-  //     logger.info(response.body)
+        logger.info('Step test: response ')
+        logger.info(response.body)
 
-  //     expect(response.body.status).toEqual(true)
-  //   })
-  // })
+        expect(response.body.status).toEqual(true)
+      })
+    })
 
-  // describe('Failure Cases:', () => {
-  //   test('should respond with status false and error INT-33, when JWS in the proof is invalid', async () => {
+    // describe('Failure Cases:', () => {
+    //   test('should respond with status false and error INT-33, when JWS in the proof is invalid', async () => {
 
-  //   })
-  // })
-  // })
+    //   })
+    // })
+  })
 })
