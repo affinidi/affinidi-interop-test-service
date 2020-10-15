2d3d1d78e0e6d23c3d12aa9b256a3053c5dece58
Afshan Aman <afshan@bloom.co>
2020-10-14 23:53:13 -0600

    changed the INTEROP_SECRET

--- apps/server/src/interop/interop.test.ts
+++ apps/server/src/interop/interop.test.ts
@@ -3,13 +3,13 @@ import supertest from 'supertest'
 import { logger } from '../shared/logger'
 
 const request = supertest(app)
-// jasmine.DEFAULT_TIMEOUT_INTERVAL = 50000
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 50000
 
 logger.info(process.env)
 logger.info(process.env.ENVIRONMENT)
 
 describe('Dummy Test', () => {
-    test('should be true', () => {
-      expect(true).toBeTruthy()
-    })
+  test('should be true', () => {
+    expect(true).toBeTruthy()
+  })
 })
