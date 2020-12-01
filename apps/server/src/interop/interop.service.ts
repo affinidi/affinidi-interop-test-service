841360cc51faaa25c881dc3248561b73095682a0
Afshan Aman <afshan@bloom.co>
2020-12-01 12:38:12 -0700

    some more console logging

--- apps/server/src/interop/interop.service.ts
+++ apps/server/src/interop/interop.service.ts
@@ -120,6 +120,10 @@ class InteropService {
         tokenUrl
       }
     } catch (e) {
+
+      console.log('interopService#generateOfferRequestToken -> catch error')
+      console.log(JSON.parse(JSON.stringify(e)))
+      
       const errorResponse = {
         status:         false,
         httpStatusCode: e.httpStatusCode,
